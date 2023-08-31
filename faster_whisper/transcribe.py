235ae47a78a383334a374b94df4914255506e225
MinorJinx <47308878+MinorJinx@users.noreply.github.com>
2023-08-31 10:13:36 -0500

    Chaning naming for duration_after_vad
    Instead of the property returning the audio duration removed, it now returns the final duration after the vad.
    If vad_filter is False or if it doesn't remove any audio, the original duration is returned.

--- faster_whisper/transcribe.py
+++ faster_whisper/transcribe.py
@@ -69,7 +69,7 @@ class TranscriptionInfo(NamedTuple):
     language: str
     language_probability: float
     duration: float
-    silence: float
+    duration_after_vad: float
     all_language_probs: Optional[List[Tuple[str, float]]]
     transcription_options: TranscriptionOptions
     vad_options: VadOptions
@@ -250,7 +250,7 @@ class WhisperModel:
             audio = decode_audio(audio, sampling_rate=sampling_rate)
 
         duration = audio.shape[0] / sampling_rate
-        silence = None
+        duration_after_vad = duration
 
         self.logger.info(
             "Processing audio with duration %s", format_timestamp(duration)
@@ -263,11 +263,11 @@ class WhisperModel:
                 vad_parameters = VadOptions(**vad_parameters)
             speech_chunks = get_speech_timestamps(audio, vad_parameters)
             audio = collect_chunks(audio, speech_chunks)
-            silence = duration - (audio.shape[0] / sampling_rate)
+            duration_after_vad = audio.shape[0] / sampling_rate
 
             self.logger.info(
                 "VAD filter removed %s of audio",
-                format_timestamp(duration - (audio.shape[0] / sampling_rate)),
+                format_timestamp(duration - duration_after_vad),
             )
 
             if self.logger.isEnabledFor(logging.DEBUG):
@@ -355,7 +355,7 @@ class WhisperModel:
             language=language,
             language_probability=language_probability,
             duration=duration,
-            silence=silence,
+            duration_after_vad=duration_after_vad,
             transcription_options=options,
             vad_options=vad_parameters,
             all_language_probs=all_language_probs,
