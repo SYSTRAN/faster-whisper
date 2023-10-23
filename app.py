251dd989acf6c9c2fb7c817da69a0c6e0eea39bb
Leah Brenny <66805316+leahbrenny@users.noreply.github.com>
2023-10-23 13:32:04 -0500

    downloads a zip containing txt and vtt

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_file
 import os
+import openai
 import shutil
+import zipfile  # Import the zipfile module
 from faster_whisper import WhisperModel
 
 app = Flask(__name__)
@@ -58,6 +60,7 @@ def transcribe_audio():
                 transcription_text = ""
 
                 for segment in segments:
+                    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
                     transcription_text += f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}\n"
 
                 transcription_results.append((audio_file.filename, transcription_text))
@@ -68,13 +71,41 @@ def transcribe_audio():
                 return f"Invalid file type: {audio_file.filename}. Supported types: .mp3 and .mp4"
 
         # Create and save .txt files with transcription results
-        for filename, transcription_text in transcription_results:
-            with open(os.path.join('temp', f'{os.path.splitext(filename)[0]}.txt'), 'w') as text_file:
-                text_file.write(transcription_text)
+        txt_file_paths = []
+        vtt_file_paths = []  # Initialize a list for .vtt file paths
 
-        # Send the .txt files as responses
-        for filename, _ in transcription_results:
-            return send_from_directory('temp', f'{os.path.splitext(filename)[0]}.txt', as_attachment=True)
+        for filename, transcription_text in transcription_results:
+            txt_file_path = os.path.join('temp', f'{os.path.splitext(filename)[0]}.txt')
+            with open(txt_file_path, 'w') as text_file:
+                text_file.write(transcription_text)
+            txt_file_paths.append(txt_file_path)
+
+            # Generate and save .vtt files (WebVTT)
+            vtt_file_path = os.path.join('temp', f'{os.path.splitext(filename)[0]}.vtt')
+            with open(vtt_file_path, 'w') as vtt_file:
+                vtt_file.write("WEBVTT\n\n")
+                segments = transcription_text.strip().split('\n')
+                for i, segment in enumerate(segments):
+                    if segment.strip():
+                        vtt_file.write(f"{i + 1}\n")
+                        vtt_file.write(f"{segment}\n\n")
+            vtt_file_paths.append(vtt_file_path)
+
+        # Automatically download the .txt and .vtt files
+        response = None
+        if len(txt_file_paths) == 1:
+            response = send_file(txt_file_paths[0], as_attachment=True)
+        elif len(txt_file_paths) > 1:
+            # Create a ZIP archive with the .txt and .vtt files if there are multiple
+            zip_filename = 'transcriptions.zip'
+            with zipfile.ZipFile(zip_filename, 'w') as zipf:
+                for txt_file_path in txt_file_paths:
+                    zipf.write(txt_file_path, os.path.basename(txt_file_path))
+                for vtt_file_path in vtt_file_paths:
+                    zipf.write(vtt_file_path, os.path.basename(vtt_file_path))
+            response = send_file(zip_filename, as_attachment=True)
+
+        return response
 
     except Exception as e:
         # Handle exceptions here
