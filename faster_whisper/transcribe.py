SYSTRAN/faster-whisper

commit c0d93d0829ccbf658e68b6b80ec0c2650bc902a8

Avoid computing higher temperatures on no_speech segments (#225)
Port commit https://github.com/openai/whisper/commit/e334ff141d5444fbf6904edaaf408e5b0b416fe8

faster_whisper/transcribe.py
@@ -649,6 +649,12 @@ class WhisperModel:
                     options.log_prob_threshold,
                 )
 
+            if (
+                options.no_speech_threshold is not None
+                and result.no_speech_prob > options.no_speech_threshold
+            ):
+                needs_fallback = False  # silence
+
             if not needs_fallback:
                 break
 

commit 2a0b49e9bff5e83485ae09c943ff95be1195f4f6

Bugfix: Illogical "Avoid computing higher temperatures on no_speech"
Fixing: https://github.com/SYSTRAN/faster-whisper/issues/621
Regression since PR: https://github.com/SYSTRAN/faster-whisper/pull/225

**The bug**: It's "silence" when decoding has failed due to `compression_ratio_threshold` [+no_speech_threshold] in https://github.com/SYSTRAN/faster-whisper/pull/225, when further down the code it's not "silence" anymore.

"Silence" should be only when decoding has failed due to `log_prob_threshold`  [+no_speech_threshold].

faster_whisper/transcribe.py
@@ -721,6 +721,7 @@ class WhisperModel:
             if (
                 options.no_speech_threshold is not None
                 and result.no_speech_prob > options.no_speech_threshold
+                and avg_logprob < options.log_prob_threshold
             ):
                 needs_fallback = False  # silence
 