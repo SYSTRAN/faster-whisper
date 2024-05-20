f53be1e81188327bd5c34e3f026e8e306a12b236
Napuh <55241721+Napuh@users.noreply.github.com>
2024-05-20 03:51:22 +0200

    Add distil models to WhisperModel init and download_model docstrings (#847)
    * chore: add distil models to WhisperModel init docstring and download_model docstring

--- faster_whisper/transcribe.py
+++ faster_whisper/transcribe.py
@@ -100,7 +100,8 @@ class WhisperModel:
 
         Args:
           model_size_or_path: Size of the model to use (tiny, tiny.en, base, base.en,
-            small, small.en, medium, medium.en, large-v1, large-v2, large-v3, or large), a path to a
+            small, small.en, distil-small.en, medium, medium.en, distil-medium.en, large-v1,
+            large-v2, large-v3, large, distil-large-v2 or distil-large-v3), a path to a
             converted model directory, or a CTranslate2-converted Whisper model ID from the HF Hub.
             When a size or a model ID is configured, the converted model is downloaded
             from the Hugging Face Hub.
