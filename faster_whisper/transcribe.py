c9e5f3bdbeec065fcf09b7bed0ad7a020a1e9985
jiltseb <jiltsebastian@gmail.com>
2024-05-24 12:35:28 +0000

    making default vad_device same as asr model device

--- faster_whisper/transcribe.py
+++ faster_whisper/transcribe.py
@@ -137,7 +137,7 @@ class BatchedInferencePipeline(Pipeline):
         options: Optional[NamedTuple] = None,
         tokenizer=None,
         device: Union[int, str, "torch.device"] = -1,
-        vad_device: Union[int, str, "torch.device"] = "cuda",
+        vad_device: Union[int, str, "torch.device"] = "auto",
         framework="pt",
         language: Optional[str] = None,
         **kwargs,
@@ -168,7 +168,6 @@ class BatchedInferencePipeline(Pipeline):
             self.device = device
 
         if self.use_vad_model:
-            # Separate vad_device from pipeline self.device
             self.vad_device = self.get_device(vad_device)
 
             # load vad model and perform VAD preprocessing if needed
@@ -189,6 +188,11 @@ class BatchedInferencePipeline(Pipeline):
         if isinstance(device, torch.device):
             return device
         elif isinstance(device, str):
+            if device == "auto":
+                if device == self.model.device:
+                    device = "cuda" if torch.cuda.is_available() else "cpu"
+                else:
+                    device = self.model.device
             return torch.device(device)
         elif device < 0:
             return torch.device("cpu")
@@ -683,12 +687,12 @@ class WhisperModel:
                 local_files_only=local_files_only,
                 cache_dir=download_root,
             )
-
+        self.device = device
         # set the random seed to make sure consistency across runs
         ctranslate2.set_random_seed(42)
         self.model = ctranslate2.models.Whisper(
             model_path,
-            device=device,
+            device=self.device,
             device_index=device_index,
             compute_type=compute_type,
             intra_threads=cpu_threads,
