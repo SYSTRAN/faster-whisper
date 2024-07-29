266a6a01ae78c68a4a9bcc2d3318838b6cc0c818
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2024-07-29 15:58:28 +0300

    enable zero silence threshold

--- faster_whisper/vad.py
+++ faster_whisper/vad.py
@@ -288,6 +288,8 @@ def get_active_regions(
                 start = timestamp["start"]
                 is_active = True
                 silence_counter = 0
+                curr_scores.append(score)
+                curr_timestamps.append(timestamp)
 
     # if active at the end, add final region
     if is_active:
