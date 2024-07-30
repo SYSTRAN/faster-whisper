dcf7eb62306e1afa2c8b8dc71a67246688d9fb26
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2024-07-30 19:50:20 +0300

    fix tests

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -39,8 +39,10 @@ def test_transcribe(jfk_path):
     assert segment.text == "".join(word.word for word in segment.words)
     assert segment.start == segment.words[0].start
     assert segment.end == segment.words[-1].end
-    batched_model = BatchedInferencePipeline(model=model, use_vad_model=False)
-    result, info = batched_model.transcribe(jfk_path, word_timestamps=True)
+    batched_model = BatchedInferencePipeline(model=model)
+    result, info = batched_model.transcribe(
+        jfk_path, word_timestamps=True, vad_filter=False
+    )
     assert info.language == "en"
     assert info.language_probability > 0.7
     segments = []
