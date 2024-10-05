dfe350796026b5af5a14d0600ad708d6d15058d7
BBC-Esq <bbc@chintellalaw.com>
2024-10-04 20:35:01 -0400

    Update utils.py
    tiny speedup
    
    perform division and modulus operations same time

--- faster_whisper/utils.py
+++ faster_whisper/utils.py
@@ -126,16 +126,11 @@ def format_timestamp(
     decimal_marker: str = ".",
 ) -> str:
     assert seconds >= 0, "non-negative timestamp expected"
-    milliseconds = round(seconds * 1000.0)
+    milliseconds = round(seconds * 1000)
 
-    hours = milliseconds // 3_600_000
-    milliseconds -= hours * 3_600_000
-
-    minutes = milliseconds // 60_000
-    milliseconds -= minutes * 60_000
-
-    seconds = milliseconds // 1_000
-    milliseconds -= seconds * 1_000
+    hours, milliseconds = divmod(milliseconds, 3_600_000)
+    minutes, milliseconds = divmod(milliseconds, 60_000)
+    seconds, milliseconds = divmod(milliseconds, 1_000)
 
     hours_marker = f"{hours:02d}:" if always_include_hours or hours > 0 else ""
     return (
