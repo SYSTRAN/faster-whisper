09d1d413044d31c5b2feffaa33cfcb14ad29dd79
Mahmoud Ashraf <hassouna97.ma@gmail.com>
2025-06-02 14:06:59 +0300

    Support `distil-large-v3.5`

--- faster_whisper/utils.py
+++ faster_whisper/utils.py
@@ -26,6 +26,7 @@ _MODELS = {
     "distil-medium.en": "Systran/faster-distil-whisper-medium.en",
     "distil-small.en": "Systran/faster-distil-whisper-small.en",
     "distil-large-v3": "Systran/faster-distil-whisper-large-v3",
+    "distil-large-v3.5": "distil-whisper/distil-large-v3.5-ct2",
     "large-v3-turbo": "mobiuslabsgmbh/faster-whisper-large-v3-turbo",
     "turbo": "mobiuslabsgmbh/faster-whisper-large-v3-turbo",
 }
